5bdd6e98482e6817a120f5a3ad98b42c52ba57d5
pdavidow <pdavidow@fastmail.fm>
2017-05-11 14:36:10 -0400

    [                                                  ] - 0 / 2elm-make: It looks like something went wrong w ith the type inference algorithm.
    Unable to generalize a type variable. It is not unranked.
    
    Please create a minimal example that triggers this problem and report it to
    <https://github.com/elm-lang/elm-compiler/issues>
    elm-make: thread blocked indefinitely in an MVar operation

--- Main.elm
+++ Main.elm
@@ -4,86 +4,27 @@ elm-make Main.elm --output elm.js
 
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text, hr, input)
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes as A exposing (style, type_, value)
-
-
-import BTree exposing (..)
+import Html exposing (Html, div)
+import TreeDiagram exposing (node, Tree)
 import BTreeView exposing (treeDiagram)
 ------------------------------------------------
 
 
-type alias Model =
-    { tree : BTree Int
-    , exponent : Int
-    , delta : Int
-    }
+type alias Model = TreeDiagram.Tree (Maybe a)
 
 
 initialModel: Model
-initialModel =
-    { tree = fromList [1, 2, 3]
-    , delta = 1
-    , exponent = 2
-    }
+initialModel = TreeDiagram.node Nothing []
 
 
-type Msg = Increment | Decrement | Raise | Delta String | Exponent String | Reset
-
-
-view : Model -> Html Msg
+view : Model -> Html
 view model =
-  div []
-    [ button [ onClick Increment ] [ text "+" ]
-    , button [ onClick Decrement ] [ text "-" ]
-    , button [ onClick Raise ] [ text "^exp" ]
-    , text "Delta: ", input [ type_ "number", A.max "1", value (toString model.delta), onInput Delta ] []
-    , text "Exponent: ", input [ type_ "number", A.min "1", value (toString model.exponent), onInput Exponent ] []
-    , button [ onClick Reset ] [ text "reset" ]
-    , div [] [ text (toString model) ]
-    , treeDiagram (asDiagramTree model.tree)
-    , hr [] []
-    , div [] [ text ("Depth: " ++ toString (depth model.tree)) ]
-    , div [] [ text ("Sum: " ++ toString (sum model.tree)) ]
-    ]
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Increment ->
-            modelWithMappedTree (\n -> n + model.delta) model
-
-        Decrement ->
-            modelWithMappedTree (\n -> n - model.delta) model
-
-        Raise ->
-            modelWithMappedTree (\n -> n ^ model.exponent) model
-
-        Delta s ->
-            {model | delta = intFromInput s}
-
-        Exponent s ->
-            {model | exponent = intFromInput s}
-
-        Reset ->
-            initialModel
-
-
-modelWithMappedTree : (Int -> Int) -> Model -> Model
-modelWithMappedTree func model =
-    {model | tree = map func model.tree}
-
-
-intFromInput : String -> Int
-intFromInput string =
-    Result.withDefault 0 (String.toInt string)
+  div [] [ treeDiagram model]
 
 
 main =
     Html.beginnerProgram
          { model = initialModel
          , view = view
-         , update = update
+         , update = initialModel
          }
