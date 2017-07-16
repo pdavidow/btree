b92cf19cc0c50d35fe8fc9f5aa8e2e3e7e930a82
pdavidow <pdavidow@fastmail.fm>
2017-07-16 02:57:39 -0400

    test BTreeVariedType.removeDuplicates

--- tests/BTreeVariedType_Tests.elm
+++ tests/BTreeVariedType_Tests.elm
@@ -1,6 +1,6 @@
 module BTreeVariedType_Tests exposing (..)
 
-import BTreeVariedType exposing (BTreeVariedType(..), toStringLength, toIsIntPrime, incrementNodes, decrementNodes, raiseNodes)
+import BTreeVariedType exposing (BTreeVariedType(..), toStringLength, toIsIntPrime, incrementNodes, decrementNodes, raiseNodes, removeDuplicates)
 import BTree exposing (..)
 import MusicNote exposing (MusicNote(..))
 import MusicNotePlayer exposing (on)
@@ -68,5 +68,31 @@ bTreeVariedType =
             , test "of 4 values" <|
                 \() ->
                     Expect.equal (BTreeVaried (Node (StringNode "abcde ^3") (singleton (IntNode 8)) (Node (MusicNoteNode (MusicNotePlayer.on E)) (singleton (BoolNode False)) Empty))) (BTreeVariedType.raiseNodes 3 (BTreeVaried (Node (StringNode "abcde") (singleton (IntNode 2)) (Node (MusicNoteNode (MusicNotePlayer.on E)) (singleton (BoolNode True)) Empty))))
+            ]
+         , describe "BTreeVariedType.removeDuplicates"
+            [ test "of empty" <|
+                \() ->
+                    Expect.equal (BTreeVaried Empty) (BTreeVariedType.removeDuplicates (BTreeVaried Empty))
+            , test "of singleton" <|
+                \() ->
+                    Expect.equal (BTreeVaried (singleton (IntNode 2))) (BTreeVariedType.removeDuplicates (BTreeVaried (singleton (IntNode 2))))
+            , test "of StringNode A, MusicNoteNode... A" <|
+                \() ->
+                     Expect.equal (BTreeVaried (Node (StringNode "A") (singleton (MusicNoteNode (MusicNotePlayer.on A))) Empty)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node (StringNode "A") (singleton (MusicNoteNode (MusicNotePlayer.on A))) Empty)))
+            , test "of IntNode" <|
+                \() ->
+                     Expect.equal (BTreeVaried (Node (IntNode 2) (singleton (IntNode 1)) Empty)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node (IntNode 2) (singleton (IntNode 2)) (singleton (IntNode 1)))))
+            , test "of StringNode" <|
+                \() ->
+                     Expect.equal (BTreeVaried (Node (StringNode "B") (singleton (StringNode "A")) Empty)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node (StringNode "B") (singleton (StringNode "B")) (singleton (StringNode "A")))))
+            , test "of BoolNode" <|
+                \() ->
+                     Expect.equal (BTreeVaried (Node (BoolNode True) (singleton (BoolNode False)) Empty)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node (BoolNode True) (singleton (BoolNode True)) (singleton (BoolNode False)))))
+            , test "of MusicNoteNode" <|
+                \() ->
+                     Expect.equal (BTreeVaried (Node (MusicNoteNode (MusicNotePlayer.on B)) (singleton (MusicNoteNode (MusicNotePlayer.on A))) Empty)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node (MusicNoteNode (MusicNotePlayer.on B)) (singleton (MusicNoteNode (MusicNotePlayer.on B))) (singleton (MusicNoteNode (MusicNotePlayer.on A))))))
+            , test "of NothingNode" <|
+                \() ->
+                     Expect.equal (BTreeVaried (singleton NothingNode)) (BTreeVariedType.removeDuplicates (BTreeVaried (Node NothingNode (singleton NothingNode) (singleton NothingNode))))
             ]
         ]
